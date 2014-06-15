54b1e2fd88f72efbf107788cf3765734a5be8213
rakib hasan <rakib063049@gmail.com>
2014-06-16 02:28:47 +0600

    Fix# two way authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,5 +25,5 @@ CSV.foreach(Rails.root.join("states.csv"), headers: true) do |row|
 end
 
 puts "Creating admin user..."
-admin=User.new(first_name: 'kyle', last_name: 'Covell', title: "Site Admin", roles_mask: 1, "email" => "siteadmin@kylecovel.com", password: '1234567', password_confirmation: '1234567')
+admin=User.new(first_name: 'kyle', last_name: 'Covell', title: "Site Admin", roles_mask: 1, email: "siteadmin@kylecovel.com", password: '1234567', password_confirmation: '1234567')
 admin.save(validate: false)
