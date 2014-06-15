7bc534b9ecfad3392fc66e9b4eebc34bb4158990
rakib hasan <rakib063049@gmail.com>
2014-06-16 01:22:03 +0600

    Seed  new admin with valid email address

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,5 @@ end
 puts "Creating admin user..."
 admin=User.new(first_name: 'kyle', last_name: 'Covell', title: "Site Admin", roles_mask: 1, "email" => "siteadmin@kylecovel.com", password: '1234567', password_confirmation: '1234567')
 admin.save(validate: false)
+admin2=User.new(first_name: 'Rakib', last_name: 'Hasan', title: "Site Admin", roles_mask: 1, "email" => "rakib063049@gmail.com", password: '1234567', password_confirmation: '1234567')
+admin2.save(validate: false)
