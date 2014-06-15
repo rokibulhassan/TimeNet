86105070b730650c05ffdc15d18adb826139a0b2
rakib hasan <rakib063049@gmail.com>
2014-06-16 01:10:53 +0600

    Generate opt secret key for existing users

--- db/migrate/20140615185444_add_opt_secret_key_for_users.rb
+++ db/migrate/20140615185444_add_opt_secret_key_for_users.rb
@@ -1,7 +1,7 @@
 class AddOptSecretKeyForUsers < ActiveRecord::Migration
   def self.up
     User.scoped.each do |user|
-      otp_secret_key = user.admin? ? "123456" : user.set_otp_secret_key
+      otp_secret_key = user.admin? ? "qazwsx" : user.set_otp_secret_key
       user.update_column(:otp_secret_key, otp_secret_key)
     end
   end
