d469c1274130fcc0d630cecdc96c16854b204c65
rakib hasan <rakib063049@gmail.com>
2014-06-17 21:37:29 +0600

    Remove ability of non-site admin users to see table of “Request info”

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,6 +8,7 @@ class Ability
     else
       can :read, :all
     end
+
     can :manage, Customer if user.role?(:create_customers)
     can :manage, Contact if user.role?(:create_contacts)
     can :manage, Project if user.role?(:create_projects)
