0fd392f6dba5aa4c5137f0f5bc66a0058a70ea0d
rakib hasan <rakib063049@gmail.com>
2014-06-19 23:32:43 +0600

    Debug smtp error on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ KyleCovell::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.assets.debug = true
-
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
