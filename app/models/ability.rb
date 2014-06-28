5c9609599a66c10e316e883223ae856f6a8e0e32
rakib hasan <rakib063049@gmail.com>
2014-06-28 14:38:45 +0600

    Fix# Extend permission for upload file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,15 +9,15 @@ class Ability
       can :read, :all
     end
 
-    can :create, Customer if user.role?(:create_customers)
+    can [:create, :upload], Customer if user.role?(:create_customers)
     can [:edit, :update], Customer if user.role?(:edit_customers)
     can :destroy, Customer if user.role?(:destroy_customers)
 
-    can :create, Contact if user.role?(:create_contacts)
+    can [:create, :upload], Contact if user.role?(:create_contacts)
     can [:edit, :update], Contact if user.role?(:edit_contacts)
     can :destroy, Contact if user.role?(:destroy_contacts)
 
-    can :create, Project if user.role?(:create_projects)
+    can [:create, :upload], Project if user.role?(:create_projects)
     can [:edit, :update], Project if user.role?(:edit_projects)
     can :destroy, Project if user.role?(:destroy_projects)
 
